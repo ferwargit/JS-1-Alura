a851540607501db4b29593dc8e6862eea1995b1c
Fernando Warno <ferwar@hotmail.com>
2022-10-11 05:34:42 -0300

    Aula1-Introduccion al lenguaje

---
+++ 1-HolaMundo.js
@@ -0,0 +1,17 @@
+console.log('Mi primer programa');
+console.log(2+2);
+
+const nombre = "Leonardo";
+console.log(nombre);
+const edad = 40;
+console.log(edad);
+
+const nombreMadre = "María";
+const nombrePadre = "José";
+const nombreHijo = "Jesús";
+
+const edadMadre= 60;
+
+console.log(nombreMadre);
+console.log("El nombre de mi madre es " + nombreMadre);
+console.log("La edad de mi madre es " + edadMadre);
