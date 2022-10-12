bd517468c1ba9e2be10238dd99adb8d341dc71ab
Fernando Warno <ferwar@hotmail.com>
2022-10-12 04:35:41 -0300

    Conversion de Tipos de Datos

---
+++ 4-ConversionTiposDatos.js
@@ -0,0 +1,10 @@
+console.log('Conversion de Tipos de datos');
+console.log("10" + "2");
+console.log(parseInt("10.1") + parseInt("2"));
+console.log(parseFloat("10.1") + parseFloat("2"));
+
+console.log("10" / "2");
+
+// NaN: Not A Number
+console.log("Maria" / "2");
+
