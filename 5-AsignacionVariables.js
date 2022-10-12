efc37a24ea658b1da46c012329dac328d1f9b8a4
Fernando Warno <ferwar@hotmail.com>
2022-10-12 05:54:31 -0300

    Asignacion de Variables

---
+++ 5-AsignacionVariables.js
@@ -0,0 +1,36 @@
+console.log('Asignacion de Variables');
+
+// const miNombre = "Leonardo";
+let miNombre = "Leonardo";
+const miApellido = "Lacruz";
+console.log(miNombre);
+
+const miNombreCompleto = miNombre + ' ' + miApellido;
+console.log(miNombreCompleto);
+
+// Template strings
+const textoMiNombre = `Mi nombre es: ${miNombre} ${miApellido}`;
+console.log(textoMiNombre);
+
+const textoMiNombreDobles = "Mi nombre es: ${miNombre} ${miApellido}";
+console.log(textoMiNombreDobles);
+
+const textoMiNombreSimples = 'Mi nombre es: ${miNombre} ${miApellido}';
+console.log(textoMiNombreSimples);
+
+let variableTexto = "Un texto";
+console.log(variableTexto);
+
+variableTexto = "Otro texto";
+console.log(variableTexto);
+
+variableTexto = 10;
+console.log(variableTexto);
+
+variableTexto = 10.5;
+console.log(variableTexto);
+
+variableTexto = false;
+console.log(variableTexto);
+
+
