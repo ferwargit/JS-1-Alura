5ec93a5e437374135a0b4b9d91a41646ad05a4f4
Fernando Warno <ferwar@hotmail.com>
2022-10-12 03:54:19 -0300

    Variables

---
+++ 2-Variables.js
@@ -0,0 +1,18 @@
+const nombreMaria = "María";
+const NombreMaria = "Teresa";
+
+console.log(nombreMaria);
+console.log(NombreMaria);
+
+const edadMadre = 60;
+console.log(typeof(edadMadre));
+
+const pi = 3.1416;
+console.log(typeof(pi));
+
+const esSoltero = false;
+console.log(typeof(esSoltero));
+
+let miEdad = 40;
+console.log(typeof(miEdad));
+
